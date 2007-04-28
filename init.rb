7b1b68b49dc26abc0c258a481d4be0dc0385c219
Evan Weaver <evan@cloudbur.st>
2007-04-28 07:50:41 +0000

    auto_rublique
    git-svn-id: http://fauna.rubyforge.org/svn/auto_rublique/trunk@165 c1ad287d-65d5-445d-b84c-e64f8492f1e6

---
+++ init.rb
@@ -0,0 +1,1 @@
+require 'lib/auto_rublique'
---
+++ install.rb
@@ -0,0 +1,10 @@
+puts DATA.read
+__END__
+
+Auto Rublique
+Make sure you have the 'rublique' gem, as well as 'json' or 'fjson'.
+  
+I was such a shy little thing that I seldom dared to open my lips, 
+and never dared to open my heart, to anybody else.
+- Bleak House
+ 

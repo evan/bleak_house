7a504e61713061c8802d09734066bdf0b43839e6
Evan Weaver <evan@cloudbur.st>
2007-04-28 09:09:58 +0000

    intermediate commit
    git-svn-id: http://fauna.rubyforge.org/svn/auto_rublique/trunk@167 c1ad287d-65d5-445d-b84c-e64f8492f1e6

--- lib/auto_rublique.rb
+++ lib/auto_rublique.rb
@@ -1,43 +1,17 @@
 
 if ['staging', 'development'].include? RAILS_ENV
 
-  require 'dispatcher'
-  require 'rublique'
-  require 'rublique_logger'
-  RubliqueLogger.file = "#{RAILS_ROOT}/log/#{RAILS_ENV}_rublique.log"
-    
-  class RubliqueLogger
-    cattr_accessor :dispatch_count
-    self.dispatch_count = 0
-    cattr_accessor :log_interval
-    self.log_interval = 1
-  end
+  require 'initializer'
   
-  class Dispatcher
-    class << self
-#      ActiveRecord::Base.logger.warn "auto_rublique: chaining"
-      def prepare_application_with_rublique
-#        ActiveRecord::Base.logger.warn "auto_rublique: prepare"
-        prepare_application_without_rublique
-        Rublique.snapshot('core')
-      end
-      alias_method_chain :prepare_application, :rublique
-      
-      def reset_after_dispatch_with_rublique
- #       ActiveRecord::Base.logger.warn "auto_rublique: reset_after_dispatch"
-        Rublique.snapshot('controller')
-        RubliqueLogger.dispatch_count += 1
-        if RubliqueLogger.dispatch_count == RubliqueLogger.log_interval
-          RubliqueLogger.dispatch_count = 0
-          RubliqueLogger.log
-        end
-        reset_after_dispatch_without_rublique
-      end
-      alias_method_chain :reset_after_dispatch, :rublique
-    end
+  class Rails::Initializer
+    def after_initialize_with_rublique
+      after_initialize_without_rublique    
+      require 'auto_rublique_overrides'
+      ActiveRecord::Base.logger.warn "auto_rublique: enabled"
+    end    
+    alias_method_chain :after_initialize, :rublique
   end
-      
-  ActiveRecord::Base.logger.warn "auto_rublique: enabled"
+        
 else
   ActiveRecord::Base.logger.warn "auto_rublique: not enabled"
 end

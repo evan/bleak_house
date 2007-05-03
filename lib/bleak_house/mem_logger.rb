16fac60d6fc4efc0ac888c55d81722dbfb34a6b4
Evan Weaver <evan@cloudbur.st>
2007-05-03 01:40:21 +0000

    swap, not VSZ

--- lib/bleak_house/mem_logger.rb
+++ lib/bleak_house/mem_logger.rb
@@ -8,20 +8,20 @@ class BleakHouse::MemLogger
     CURRENT = {}
     TAGS = Hash.new(0)
     TIMEFORMAT = '%Y-%m-%d %H:%M:%S'
-    VSZ = "memory usage/virtual"
+    SWAP = "memory usage/swap"
     RSS = "memory usage/real"
 
     def log(path, with_mem = false)
       File.open(path, 'a+') do |log|
         log.sync = true
-        TAGS[VSZ], TAGS[RSS] = mem_usage if with_mem
+        TAGS[SWAP], TAGS[RSS] = mem_usage if with_mem
+        TAGS[SWAP] -= TAGS[RSS]
         log.puts Base64.encode64(Marshal.dump([Time.now.strftime(TIMEFORMAT), TAGS])).gsub("\n", '')
       end
       GC.start
     end
     
     def snapshot(tag)
-#      puts "bleak_house: seen: #{SEEN.size}"
       CURRENT.clear
       ObjectSpace.each_object do |obj|
         CURRENT[obj_id = obj._bleak_house_object_id] = true
@@ -31,11 +31,9 @@ class BleakHouse::MemLogger
           TAGS[SEEN[obj_id]] += 1
         end
       end
-#      puts "bleak_house: current: #{CURRENT.size}"
       SEEN.keys.each do |obj_id|
         TAGS[SEEN.delete(obj_id)] -= 1 unless CURRENT[obj_id]          
       end
- #     puts "bleak_house: done"
       CURRENT.clear
       GC.start
     end        

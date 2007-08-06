cb56bb26579845465614fbf95eb6b690f2af4270
Evan Weaver <evan@cloudbur.st>
2007-08-06 22:20:53 +0000

    fix tests

--- test/unit/test_bleak_house.rb
+++ test/unit/test_bleak_house.rb
@@ -6,9 +6,7 @@ require 'test/unit'
 require 'yaml'
   
 class BleakHouseTest < Test::Unit::TestCase
-  require "#{DIR}lib/bleak_house/mem_logger"
   require "#{DIR}lib/bleak_house/c"
-  require "#{DIR}lib/bleak_house/ruby"
 
   SNAPSHOT_FILE = "/tmp/bleak_house"
   SNAPS = {:c => SNAPSHOT_FILE + ".c.yaml",
@@ -17,19 +15,6 @@ class BleakHouseTest < Test::Unit::TestCase
   def setup
   end
 
-  def test_mem_inspect
-    assert `ruby-bleak-house -rrubygems -e "require 'mem_inspect'; puts 'ok'"` == "ok\n"
-  end
-  
-  def test_ruby_snapshot
-    File.delete SNAPS[:ruby] rescue nil
-    ::BleakHouse::RubyLogger.new.snapshot(SNAPS[:ruby], "ruby_test", true)
-    assert File.exist?(SNAPS[:ruby])
-    assert_nothing_raised do 
-      assert YAML.load_file(SNAPS[:ruby]).is_a?(Array)
-    end
-  end
-  
   def test_c_snapshot
     File.delete SNAPS[:c] rescue nil
     ::BleakHouse::CLogger.new.snapshot(SNAPS[:c], "c_test", true)

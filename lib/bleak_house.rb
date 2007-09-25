c77af432510b21bb25932d2679ea678ffac48169
Evan Weaver <evan@cloudbur.st>
2007-09-25 06:39:39 +0000

    preparing to switch to csv
    git-svn-id: http://fauna.rubyforge.org/svn/bleak_house/branches/trace@838 c1ad287d-65d5-445d-b84c-e64f8492f1e6

--- lib/bleak_house.rb
+++ lib/bleak_house.rb
@@ -1,6 +1,7 @@
 
-if ENV['BLEAK_HOUSE']
-  require 'bleak_house/logger'
+require 'bleak_house/logger'
+
+if ENV['RAILS_ENV'] and ENV['BLEAK_HOUSE']
   require 'bleak_house/rails'
-  BleakHouse::Rails.warn "enabled (log/bleak_house_#{RAILS_ENV}.dump)"    
+  BleakHouse::Rails.warn "enabled (log/bleak_house_#{RAILS_ENV}.yaml.log)"    
 end

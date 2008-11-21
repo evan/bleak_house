4cf0bff0c477851eb9de97059f40a26d4b648a4a
Manfred Stienstra <manfred@fngtps.com>
2008-11-22 02:55:16 +0800

    Add a build log for ext.
    Create a build log from extconf.rb so it's easier to find out what went wrong.
    
    Signed-off-by: Snax Fauna <evan+fauna@cloudbur.st>

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,3 +1,23 @@
+require 'fileutils'
 
-system('ruby build_ruby.rb &> /dev/null')
-exec('ruby-bleak-house build_snapshot.rb')
+module BleakHouse
+  def self.write_to_log(filename, message)
+    File.open(filename, 'a') { |fp| fp.write("#{message}\n") }
+  end
+  
+  def self.execute(command)
+    puts "$}- #{command}"
+    exit -1 unless system(command)
+  end
+end
+
+logdir = File.expand_path('../../log', __FILE__)
+
+logfile = File.join(logdir, 'bleak_house.log')
+FileUtils.mkdir_p(logdir)
+
+BleakHouse.write_to_log(logfile, '-%{ BUILDING RUBY }%-')
+BleakHouse.execute("ruby build_ruby.rb >> #{logfile} 2>&1")
+
+BleakHouse.write_to_log(logfile, '-%{ BUILDING SNAPSHOT }%-')
+BleakHouse.execute("ruby-bleak-house build_snapshot.rb >> #{logfile} 2>&1")

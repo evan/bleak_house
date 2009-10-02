4e201cf48c9b345b65ae977c4e3fd887dfee5d6b
Evan Weaver <eweaver@twitter.com>
2009-10-02 16:26:07 -0700

    Stuff.

--- lib/bleak_house/hook.rb
+++ lib/bleak_house/hook.rb
@@ -1,11 +1,11 @@
 
 module BleakHouse
   # The body of the exit handler and <tt>SIGUSR2</tt> trap. It writes a snapshot to a dumpfile named after the current Process.pid.
-  def self.hook
+  def self.hook(gc_runs = 3)
     @count ||= 0
     filename = "/tmp/bleak.%s.%03i.dump" % [Process.pid,@count]
     STDERR.puts "** BleakHouse: working..."
-    BleakHouse.snapshot(filename)
+    BleakHouse.snapshot(filename, gc_runs)
     STDERR.puts "** BleakHouse: complete\n** Bleakhouse: Run 'bleak #{filename}' to analyze."
     @count += 1
   end
